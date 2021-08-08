6f468451b7f5b143263a3bf0671b3023138f9e5a
R4356th <R4356th@users.noreply.github.com>
2021-08-08 16:53:26 +0000

    deploy: e3af390366cd75474ec2b9012f9d18cdebfd5b90

--- chunks/gui.js
+++ chunks/gui.js
@@ -145,7 +145,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = 'https://scratch.mit.edu';
+  window.location = 'https://glitchy-scratch.js.org';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
